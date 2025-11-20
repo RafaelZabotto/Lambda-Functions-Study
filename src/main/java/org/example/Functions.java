609262c78462cd2f9871ac6ab8f3ad64b2f90422
RafaelZabotto <rafaelm.zabotto@gmail.com>
2025-11-20 17:28:01 -0300

    Lambdas no Stream

---
+++ src/main/java/org/example/Functions.java
@@ -0,0 +1,59 @@
+package org.example;
+
+import javax.swing.text.html.parser.Entity;
+import java.util.function.*;
+
+public class Functions {
+
+    /*
+      ->  Most common/used types of Lambda functions
+      1 - Function
+      2 - BiFunction
+      3 - Consumer
+      4 - BiConsumer
+      5 - Predicate
+      6 - Supplier
+     */
+
+    /*
+     * Functions receive a one argument and have one return, not necessary the same type
+     * In functions you can use some methods, commonly .apply, .andThen, .equals.
+     */
+    static Function<Integer, Integer> calculateAreaOfSquare = side -> side*side;
+
+    /*
+     * BiFunctions is like Functions but, they receive 2 arguments and have one return, not necessary the same type
+     * The methods are the same of Functions
+     */
+    static BiFunction<Integer, Integer, Integer> calculateAreaOfRetangle = (side1, side2) -> side1*side2;
+
+    /*
+    * Consumers receive one argument e returns no result.
+    * The method is accessed .accept
+    */
+
+    static Consumer<Plan> purchisingPlan = plan -> System.out.println("Congrulations you bought the " + plan.user + " plan for a " + plan.periodicy);
+
+    record Plan (String user, String planType, String periodicy) { }
+
+    /*
+    * BiConsumers are the same of Consumer but, they use two arguments
+    * The methods are the same of Consumer.
+    */
+
+    static BiConsumer<Plan, Boolean> checkIfIsRegistred = ((plan, aBoolean) ->
+            System.out.println((aBoolean ? "Hello " + plan.user + " , you are already registred in plan " + plan.planType + " for a " + plan.periodicy :
+                    "You are not registered yet, please choose a plan")));
+
+
+    /*
+    * Predicate is a boolean function with one argument
+    * To access the predicate function we need to use the .test method
+    */
+
+    static Predicate<Plan> checkIfIsAPremiumUser = plan -> plan.planType().equals("Gold");
+
+    /*
+    * Suplier
+    */
+}
