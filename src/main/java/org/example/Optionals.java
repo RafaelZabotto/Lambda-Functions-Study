63d80a845c04960c67bf585c9ce2ee2747f02986
RafaelZabotto <rafaelm.zabotto@gmail.com>
2025-11-25 17:38:19 -0300

    Optionals

--- src/main/java/org/example/Optionals.java
+++ src/main/java/org/example/Optionals.java
@@ -1,10 +1,25 @@
 package org.example;
 
+import java.util.Optional;
+
 public class Optionals {
 
     //Options is must important for prevent NullPointExceptions
     public static void main(String[] args) {
 
+        // Here we have two types of return, first when the optional receives a NULL the return is  "default value"
+        // But, if the value is not null, the optional will return the value
 
+        Object value = Optional.ofNullable(null).orElseGet(() -> "default value");
+
+        System.out.println(value);
+
+        // In this example, it you throw a exception if NULL
+        Object value1 = Optional.ofNullable("hello").orElseThrow(RuntimeException::new);
+
+        System.out.println(value1);
+
+        //In this case, if value is not null, do something
+        Optional.of(null).isPresent();
     }
 }
